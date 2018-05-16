7a44308eb9dac30493879e8fb0baf457788ecf13
uservGitHub <userv@126.com>
2018-05-16 10:53:05 +0800

    引导界面

--- app/src/main/java/gxd/book/BookBootstrapActivity.kt
+++ app/src/main/java/gxd/book/BookBootstrapActivity.kt
@@ -2,18 +2,12 @@ package gxd.book
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
-import android.widget.LinearLayout
+import android.view.Gravity
+import android.view.View
 import android.widget.TextView
-import com.example.work.myapplication.model.Cat
-import com.example.work.myapplication.model.Dog
-import com.example.work.myapplication.model.Person
-import io.realm.Realm
-import io.realm.Sort
-import io.realm.kotlin.createObject
-import io.realm.kotlin.where
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
+import org.jetbrains.anko.ctx
+import org.jetbrains.anko.dip
+import org.jetbrains.anko.sp
 
 /**
  * Created by work on 2018/5/16.
@@ -22,8 +16,10 @@ import org.jetbrains.anko.uiThread
 class BookBootstrapActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val headView = TextView().apply{
-            text = "引导界面"
+        val headView = TextView(ctx).apply {
+            text = "引导主界面"
+            textSize = sp(30).toFloat()
+            gravity = Gravity.CENTER
         }
         setContentView(headView)
         //setContentView(R.layout.activity_realm_basic_example)
