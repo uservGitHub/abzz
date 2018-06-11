ba26d3130173f9d1120f040437cdef4733cb09a6
uservGitHub <userv@126.com>
2018-06-11 13:56:45 +0800

    调试前备份

--- app/src/main/java/gxd/book/business/VerticalSlideManager.kt
+++ app/src/main/java/gxd/book/business/VerticalSlideManager.kt
@@ -4,16 +4,17 @@ import android.animation.Animator
 import android.animation.AnimatorListenerAdapter
 import android.animation.ValueAnimator
 import android.content.Context
-import android.graphics.Bitmap
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
+import android.graphics.*
 import android.util.Log
 import android.view.GestureDetector
 import android.view.MotionEvent
 import android.view.View
 import android.view.animation.DecelerateInterpolator
 import android.widget.OverScroller
+import gxd.book.android.sdPath
+import io.reactivex.Observable
+import io.reactivex.Scheduler
+import io.reactivex.schedulers.Schedulers
 import org.jetbrains.anko.AnkoLogger
 import org.jetbrains.anko.info
 import java.util.*
@@ -24,20 +25,319 @@ private val randColor:Int get() {
     return Color.rgb(r.nextInt(256), r.nextInt(256), r.nextInt(256))
 }
 
-class HostView(ctx:Context):View(ctx){
-    internal fun moveTo(x:Float, y:Float) = Unit
-    internal fun moveToX(x:Float) = Unit
-    internal fun moveToY(y:Float) = Unit
+class HostView(ctx:Context):View(ctx),AnkoLogger{
+    override val loggerTag: String
+        get() = VIEW_TAG
+    private val animationManger:AnimationManger
+    private val dragPinchManager:DragPinchManager
+    internal fun moveTo(x:Float, y:Float) {
+        visRects.forEach {
+            if (it.flagHiting || !it.locked){
+                it.worldX = x
+                it.worldY = y
+            }
+        }
+        invalidate()
+    }
+    internal fun moveToX(x:Float) = moveTo(x, visY)
+    internal fun moveToY(y:Float) = moveTo(visX, y)
 
     //dragpinch
-    internal fun hiting(e:MotionEvent):Boolean = true
-    internal val isSwipeVertical = false
-    internal fun moveOffset(dx:Float, dy:Float) = Unit
-    internal val visX:Float = 0F
-    internal val visY:Float = 0F
+    internal fun hiting(e:MotionEvent){
+        visRects.forEach {
+            it.flagHiting = it.clipRect.contains(e.x, e.y)
+        }
+    }
+    internal val isSwipeVertical = true
+    internal fun moveOffset(dx:Float, dy:Float) = moveTo(visX+dx, visY+dy)
+    internal val visX:Float get() = selectedVisRect.worldX
+    internal val visY:Float get() = selectedVisRect.worldY
     internal val visBoundX:Float = 0F
     internal val visBoundY:Float = 0F
+    private inline val selectedVisRect:VisRect get() = visRects.first()
+
+    private var hasSize = false
+    private var visType:Int = VisRect.FILL_ALL
+    internal val visRects = mutableListOf<VisRect>()
+    init {
+        setWillNotDraw(false)
+        animationManger = AnimationManger(this)
+        dragPinchManager = DragPinchManager(this, animationManger)
+        //visRects.add(VisRect())
+    }
+    private fun createPageManager():PageManager{
+        val filename = "${context.sdPath}/gxd.book/atest/testpdf.pdf"
+        val pdfFile = PdfFile(filename)
+        pdfFile.openFile()
+        pdfFile.openDoc()
+        val pageManager = PageManager(width,pdfFile).apply {
+            setOnLoadFinished {
+                dragPinchManager.enable()
+                invalidate()
+            }
+        }
+        return pageManager
+    }
+
+
+    //region internal
+    /**
+     * 添加、调整布局、刷新
+     */
+    internal fun autoAdd() {
+        if (visRects.size != 1) {
+            return
+        }
+        visRects.add(VisRect(createPageManager()))
+        updateVisRectLayout()
+        invalidate()
+    }
+
+    /**
+     * 删除、调整布局、刷新
+     */
+    internal fun remove(visRect: VisRect){
+        if (visRects.size != 2){
+            return
+        }
+        visRects.remove(visRect)
+        updateVisRectLayout()
+        invalidate()
+    }
+
+    internal fun reverse(){
+        if (visRects.size != 2){
+            return
+        }
+        updateVisRectLayout(true)
+        invalidate()
+    }
+
+    /**
+     * 调整布局
+     */
+    private fun updateVisRectLayout(reverse:Boolean = false, isAuto:Boolean = true){
+        when{
+            reverse -> {
+                visType = if (visType == VisRect.HOR_ALL)VisRect.VER_ALL else VisRect.HOR_ALL
+            }
+            isAuto -> {
+                if (height == width){
+                    info { "visType=0" }
+                    visType = VisRect.FILL_ALL
+                }else {
+                    visType = if (height > width) VisRect.VER_ALL else VisRect.HOR_ALL
+                }
+            }
+        }
+        val rectF = RectF(0F, 0F, width.toFloat(), height.toFloat())
+        info { "updateVisLayout(count=${visRects.size},visType=$visType)" }
+        VisRect.nativeArrangeVisRectLayout(visRects, rectF, visType)
+    }
+
+    /**
+     * 回弹
+     */
+    internal fun springBack() {
+        /*if (isSwipeVertical) {
+            if (visBoundY != visY) {
+                animationManager.startYAnimation(visY, visBoundY, true)
+            }
+        } else {
+            if (visBoundX != visX) {
+                animationManager.startXAnimation(visX, visBoundX, true)
+            }
+        }*/
+    }
+    //endregion
+
+    //region    override
+
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        hasSize = true
+        super.onSizeChanged(w, h, oldw, oldh)
+        if (isInEditMode) {
+            return
+        }
+        animationManger.stopAll()
+        if (visRects.size == 0){
+            visRects.add(VisRect(createPageManager()).apply {
+                flagHiting = true
+            })
+        }
+        updateVisRectLayout(false, false)
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        if (isInEditMode) {
+            return
+        }
+
+        //region    draw background
+        var isDrawBackground = false
+        background?.let {
+            isDrawBackground = true
+            it.draw(canvas)
+        }
+        if (!isDrawBackground) {
+            canvas.drawColor(Color.YELLOW)
+        }
+        //endregion
+
+        val paint = Paint()
+        canvas.drawFilter = PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
+
+        visRects.forEachIndexed { index, visRect ->
+            //val rect = Rect(dt, dt, width - dt, height - dt)
+            info { visRect }
+            visRect.look(canvas){
+                visRect.pageManager.renderPages(it, paint,
+                        visRect.worldX,visRect.worldY, visRect.height.toInt())
+                //canvas.drawRect(rect, Paint().also { it.color = visRect.testColor })
+                /*val paint = textPaintFrom(visRect.testColor, sp(fontSize + 4*(index+1)).toFloat())
+                canvas.drawText(
+                        "$index:(${visX.toInt()},${visY.toInt()}),visRect$index:($visRect)",
+                        visRect.worldX+visRect.clipX,visRect.worldY+visRect.clipY,paint)*/
+            }
+        }
+    }
+
+    override fun computeScroll() {
+        super.computeScroll()
+        if (isInEditMode) {
+            return
+        }
+        animationManger.computeFling()
+    }
+    //endregion
 }
+
+//region    VisRect
+class VisRect(val pageManager: PageManager,val testColor: Int = randColor){
+    var width: Float = 0F
+    var height: Float = 0F
+    var clipX: Float = 0F
+    var clipY: Float = 0F
+
+    val boundaryMin:Float get() = pageManager.startBoundarySlide
+    val boundaryMax :Float get() = pageManager.endBoundarySlide
+
+    companion object {
+        /**
+         * 水平排列，从左向右，等分
+         */
+        val FILL_ALL = 0    //全屏布置
+        val HOR_ALL = 1 //水平排列，从左向右，等分
+        val VER_ALL = 2 //垂直排列，从上向下，等分
+
+        val MOVE_CLIP = 1   //移动窗口
+        val MOVE_WORLD = 2  //移动世界坐标系
+
+        val MOVE_CLIP_L = 1   //移动左
+
+        val MOVE_CLIP_T = 2   //移动左
+
+        //region    native模型函数
+        /**
+         * 重新排列所有可视区
+         */
+        internal fun nativeArrangeVisRectLayout(list: List<VisRect>, rect: RectF, type: Int) {
+
+            val count = list.size
+            if (count == 0) {
+                return
+            }
+
+            when (type) {
+                VisRect.HOR_ALL -> {
+                    val dWidth = rect.width() / count
+                    var x = rect.left
+                    list.forEachIndexed { index, visRect ->
+                        visRect.also {
+                            it.clipX = x
+                            it.clipY = rect.top
+                            it.width = dWidth
+                            it.height = rect.height()
+                            //这个值添加的时候设置
+                            //it.zIndex = index + 1 //从1开始
+                        }
+                        x += dWidth
+                    }
+                }
+                VisRect.VER_ALL -> {
+                    val dHeight = rect.height() / count
+                    var y = rect.top
+                    list.forEachIndexed { index, visRect ->
+                        visRect.also {
+                            it.clipX = 0F
+                            it.clipY = y
+                            it.width = rect.width()
+                            it.height = dHeight
+                            //这个值添加的时候设置
+                            //it.zIndex = index + 1 //从1开始
+                        }
+                        y += dHeight
+                    }
+                }
+                VisRect.FILL_ALL ->{
+                    list.forEachIndexed { index, visRect ->
+                        visRect.also {
+                            it.clipX = 0F
+                            it.clipY = 0F
+                            it.width = rect.width()
+                            it.height = rect.height()
+                            //这个值添加的时候设置
+                            //it.zIndex = index + 1 //从1开始
+                        }
+                    }
+                }
+            }
+        }
+        //endregion
+    }
+
+    var worldX: Float = 0F
+    var worldY: Float = 0F
+    var zIndex = 0
+    var locked = true   //不随动
+    var flagHiting = false  //点击命中，选中，只能有一个选中
+    //var flagWorldMoving = false //世界坐标系可移动
+    //var flagClipMoving = false //视区起点可移动
+    val clipRect: RectF get() = RectF(clipX, clipY, clipX + width, clipY + height)
+
+    fun look(canvas: Canvas, f: (Canvas) -> Unit) {
+        canvas.save()
+        canvas.clipRect(clipRect)   //指定区域
+        if (flagHiting) {
+            //canvas.drawColor(Color.LTGRAY)
+        }
+        //look的位置
+        //canvas.translate(-worldX, -worldY)
+        //draw world
+        f.invoke(canvas)
+        canvas.restore()
+    }
+
+    fun moveWorldOffset(dx: Float, dy: Float) {
+        worldX += dx
+        worldY += dy
+    }
+
+    fun moveClipOffset(dx: Float, dy: Float) {
+        //要求内容world也要随之移动
+        clipX += dx
+        clipY += dy
+        moveWorldOffset(-dx, -dy)
+    }
+
+    override fun toString(): String {
+        return "clip:$clipRect,world:${PointF(worldX,worldY)}"
+    }
+}
+
+
+//endregion
 
 //修改加速滑动：只能向一个方向
 class AnimationManger(private val host:HostView): AnkoLogger {
@@ -297,6 +597,13 @@ class DragPinchManager(private val host: HostView, private val animationManger: 
 
 //endregion
 
+/**
+ * 页管理，核心概念：
+ * 1 绘制时，自动进行页的异步加载和删除
+ * 2 提供世界坐标系的可变界值
+ * 3 提供页码的范围
+ * 注意：绘制时，只需传递可视起点在世界坐标系中的位置和窗口长度
+ */
 class PageManager private constructor(
         val isVertical: Boolean,
         sideLength:Int,
@@ -304,7 +611,7 @@ class PageManager private constructor(
         startPageInd:Int):AnkoLogger {
     override val loggerTag: String
         get() = "_View"
-    constructor(length: Int, pdf: PdfFile) : this(true, length, pdf, 21)
+    constructor(length: Int, pdf: PdfFile, startPageInd: Int = 0) : this(true, length, pdf, startPageInd)
 
     var sideLength: Int
         private set
@@ -318,9 +625,11 @@ class PageManager private constructor(
         private set
     private var endBoundaryPageInd: Int = -1
         private set
+    //var requestPage:io.reactivex.Observable<out Int>? = null
 
     private val maxPageCount = 12
-    private val initPageCount = 10
+    private val initPageCount = 2
+    private val pageCountPerTime = 2
     private val pageListUpdateLock = Any()
     private var loadFinish:(()->Unit)? = null
     @Volatile
@@ -335,29 +644,39 @@ class PageManager private constructor(
         backwardAdd(startPageInd, initPageCount)
     }
 
-    fun renderPages(canvas: Canvas, visX:Float, visY:Float, visLength:Int){
-        val visValue = if (isVertical) visY else visX
-        val visEnd = visValue + visLength
-        val begInd = pageList.indexOfFirst { it.begAnixValue<=visEnd && it.endAnxiValue>= visValue}
-        val endInd = pageList.indexOfLast { it.begAnixValue<=visEnd && it.endAnxiValue>= visValue }
+    fun renderPages(canvas: Canvas, paint:Paint, visX:Float, visY:Float, visLength:Int) {
+        if (pageList.size == 0) {
+            return
+        }
 
-        info { "begInd,endInd = $begInd,$endInd ${visValue.toInt()},${visEnd.toInt()}" }
-        if (begInd == -1 || endInd == -1) return
+        val visStart = if (isVertical) visY else visX
+        val visEnd = visStart + visLength
 
-        info { "${pageList[begInd].begAnixValue},${pageList[endInd].endAnxiValue}" }
-        //val antialiasFilter = PaintFlagsDrawFilter(0, Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
-        val paint = Paint()
-        //canvas.drawFilter = antialiasFilter
+        //无交集返回
+        if (startBoundarySlide >= visEnd || endBoundarySlide <= visStart) {
+            return
+        }
 
-        try {
-            canvas.translate(-visX, -visY)
-            for (ind in begInd..endInd) {
-                val page = pageList[ind]
-                canvas.drawBitmap(page.bmp, visX, page.begAnixValue, paint)
+        canvas.translate(-visStart, -visEnd)
+        synchronized(pageListUpdateLock) {
+            pageList.forEach { page ->
+                if (visStart >= page.begAnixValue && visStart < page.endAnixValue) {
+                    canvas.drawBitmap(page.bmp, visX, page.begAnixValue, paint)
+                } else if (visEnd <= page.begAnixValue) {
+                    return@forEach
+                }
             }
-            canvas.translate(visX, visY)
-        }catch (e:Exception){
-            Log.v("_View", "${e.message}")
+        }
+        canvas.translate(visStart, visEnd)
+
+        val offset = 2 * visLength
+        if (Math.abs(endBoundarySlide - visEnd) < offset && canNextPage) {
+            backwardLoad()
+            return
+        }
+        if (Math.abs(startBoundarySlide - visStart) < offset && canPrePage) {
+            forwardLoad()
+            return
         }
     }
 
@@ -369,27 +688,38 @@ class PageManager private constructor(
         if (loading || !canNextPage) return
         loading = true
 
-        backwardAdd(pageList.last().pageInd + 1)
-        if (pageList.size > maxPageCount) {
-            pageList.first().remove()
-        }
-        loadFinish?.invoke()
-        loading = false
+        Observable.just(pageList.last().pageInd+1)
+                .subscribeOn(Schedulers.io())
+                .map { backwardAdd(it, pageCountPerTime) }
+                .observeOn(Schedulers.io())
+                .subscribe {
+                    while (pageList.size > maxPageCount){
+                        pageList.first().remove()
+                    }
+                    loadFinish?.invoke()
+                    loading = false
+                }
     }
 
     fun forwardLoad() {
         if (loading || !canPrePage) return
         loading = true
 
-        forwardAdd(pageList.first().pageInd - 1)
-        if (pageList.size > maxPageCount) {
-            pageList.last().remove()
-        }
-        loadFinish?.invoke()
-        loading = false
+        Observable.just(pageList.first().pageInd - 1)
+                .subscribeOn(Schedulers.io())
+                .map { forwardAdd(it, pageCountPerTime) }
+                .observeOn(Schedulers.io())
+                .subscribe {
+                    while (pageList.size > maxPageCount){
+                        pageList.last().remove()
+                    }
+                    loadFinish?.invoke()
+                    loading = false
+                }
     }
 
-    //region    两个页边界（执行构造函数时，已经加载了一页）
+    //region    native函数，页码能否向前或向后；添加新页，前面加或后面加
+
     private val canPrePage: Boolean
         get() = when {
             pageList.first().pageInd <= 0 -> false
@@ -400,7 +730,6 @@ class PageManager private constructor(
             pageList.last().pageInd + 1 >= pdfFile.pageCount -> false
             else -> pdfFile.openPage(pageList.last().pageInd + 1)
         }
-    //endregion
 
     private fun backwardAdd(pageInd: Int, count: Int = 1) {
         val lastInd = Math.min(pageInd + count - 1, pdfFile.pageCount - 1)
@@ -418,11 +747,22 @@ class PageManager private constructor(
         }
     }
 
+    //endregion
+
+
     //创建时，自动添加到列表中；remove自动从列表中删除
+    /**
+     * 页，核心概念：
+     * 1 创建对象（由pageInd）时，自动填充，并进维护
+     * 2 删除对象（由对象实施），自动维护
+     * 3 只读的起终点（位图在世界坐标系中的可变一维）及位图
+     * 维护（重点）：页列表，页的范围[startBoundaryPageInd,endBoundaryPageInd]，
+     * 可变一维坐标系的范围[startBoundarySlide,endBoundarySlide]
+     */
     inner private class Page(val pageInd: Int) {
         var begAnixValue: Float = 0F
             private set
-        val endAnxiValue: Float
+        val endAnixValue: Float
             get() =
                 begAnixValue + if (isVertical) bmp.height else bmp.width
         val bmp: Bitmap
